4310a0ec499d45f4da60de0e3d9c2ef62cd33b56
Felix Kubicek <kubicek@uni-potsdam.de>
2014-11-19 18:18:10 +0100

    k calculations per thread

--- uebung02/blatt2_reissaus_kubicek/aufgabe_2_2/pi.c
+++ uebung02/blatt2_reissaus_kubicek/aufgabe_2_2/pi.c
@@ -20,18 +20,21 @@ thread_routine (void * arg)
 {
   int *samples_to_compute = arg;
    
-  double x = pr_random_f(CIRCLE_RADIUS);
-  double y = pr_random_f(CIRCLE_RADIUS);
-  
-  printf ("thread_id: %d; num_samples: %d; x: %f; y: %f\n", (int) pthread_self (), *samples_to_compute, x, y);
-
-  if (circle_miss(x,y))
-  {
-    pthread_mutex_lock(&circle_misses_mutex);
-    //printf("begin circle_mutex: %d\n",(int) pthread_self ()); 
-    circle_misses ++;
-    //printf("end   circle_mutex: %d\n",(int) pthread_self ());
-    pthread_mutex_unlock(&circle_misses_mutex);
+  //printf ("thread_id: %d; num_samples: %d; x: %f; y: %f\n", (int) pthread_self (), *samples_to_compute, x, y); 
+  int k;
+  for (k = 0; k < *samples_to_compute; k++)
+  {  
+    double x = pr_random_f(CIRCLE_RADIUS);
+    double y = pr_random_f(CIRCLE_RADIUS);
+ 
+    if (circle_miss(x,y))
+    {
+      pthread_mutex_lock(&circle_misses_mutex);
+      //printf("begin circle_mutex: %d\n",(int) pthread_self ()); 
+      circle_misses ++;
+      //printf("end   circle_mutex: %d\n",(int) pthread_self ());
+      pthread_mutex_unlock(&circle_misses_mutex);
+    }
   }
   
   pthread_exit(NULL);
@@ -64,7 +67,8 @@ main (int argc, char** argv)
   printf ("start number_threads: %d ,number_samples: %d\n", num_threads, num_samples);
   
   // calculating number of samples per thread
-  int samples_per_thread = num_samples / num_threads; 
+  int samples_per_thread = num_samples / num_threads;
+  int samples_per_thread_plus_one = samples_per_thread + 1;
   int remaining_samples = num_samples % num_threads;
 
   pthread_t tid[num_threads];
@@ -72,9 +76,9 @@ main (int argc, char** argv)
   int i;
   for (i = 0; i < num_threads; i++)
   {
-    int samples_to_compute = (remaining_samples > i) ? samples_per_thread + 1 : samples_per_thread;
+    int *samples_to_compute = (remaining_samples > i) ? &samples_per_thread_plus_one : &samples_per_thread;
    
-    pthread_create (&tid[i], NULL, &thread_routine, (void *) &samples_to_compute);
+    pthread_create (&tid[i], NULL, &thread_routine, (void *) samples_to_compute);
   }
 
   for (i = 0; i < num_threads; i++)
